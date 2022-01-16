aea187ebd24d8c55f5c6502c21c2b8bedbecf767
Virat <viratpatel993@gmail.com>
2022-01-17 00:54:13 +0530

    login and logo

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -15,6 +15,18 @@ const Login = () =>
     const { dispatch } = useContext(AuthContext)
     // const history = useNavigate();
 
+    const handleAdmin = () =>
+    {
+        setEmail("admin@gmail.com")
+        setPassword("admin")
+    }
+
+    const handleDemoLogin = () =>
+    {
+        setEmail("vp@gmail.com")
+        setPassword("vp")
+    }
+
     const handleLogin = (e) =>
     {
         e.preventDefault();
@@ -44,7 +56,10 @@ const Login = () =>
                     <h1>Sign In</h1>
                     <input type="email" placeholder="Email " onChange={(e) => setEmail(e.target.value)} />
                     <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                    <button className='loginButton' onClick={handleLogin}>Sign In</button>
+                    <div style={{ width: "100%" }}>
+                        <button style={{ width: "50%", fontWeight: "600" }} className='loginButton' onClick={handleDemoLogin}>Demo Sign in</button>
+                        <button style={{ width: "50%", backgroundColor: "rgb(62, 224, 12)", color: "black", fontWeight: "600" }} className='loginButton' onClick={handleAdmin}>Admin</button>
+                    </div>
                     <span>New to Netflix? <b><Link style={{ color: "white" }} to="/register">Sign up now.</Link></b></span>
                     <small>This page is protected by Google reCAPTCHA to ensure you 're not a bot. <b>Learn more</b>.</small>
                 </form>
